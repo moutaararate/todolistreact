74ef006c96ca8973bd45e24e631eff0f5ef0534e
moutaararate <moutaararate@lyra-network.com>
2020-03-23 00:14:44 +0100

    Updates

---
+++ precache-manifest.3380122e857d5cbb5125cbf86f748f5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a1c464bea62d7a469541dd24034ba83",
+    "url": "/todolistreact/index.html"
+  },
+  {
+    "revision": "3c8cf6140232ad4ad16d",
+    "url": "/todolistreact/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "3c8cf6140232ad4ad16d",
+    "url": "/todolistreact/static/js/2.f3539973.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolistreact/static/js/2.f3539973.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0a4f0054ba6762d92a6",
+    "url": "/todolistreact/static/js/main.b8cbd8f4.chunk.js"
+  },
+  {
+    "revision": "8710cd8820f9d6fa8b59",
+    "url": "/todolistreact/static/js/runtime-main.af565d87.js"
+  }
+]);
